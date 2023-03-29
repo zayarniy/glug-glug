84a9355abf4635b98397d0f72418995a458b7096
Ваше имя <zaazaa@yandex.ru>
2023-03-29 12:27:42 +0300

    Добавил управление с ограничением

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,7 +7,7 @@ public class Controller : MonoBehaviour
     // Start is called before the first frame update
     public float speed;
     Transform Ship;
-    public Transform Diver;
+    Transform Diver;
     public Vector3 DiverOffset;
 
     void Start()
@@ -20,10 +20,21 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         float move = speed * Time.deltaTime;
-        //print(new Vector3(0, Input.GetAxis("Vertical") , 0));
-        Ship.Translate(move * Input.GetAxis("Horizontal"), 0, 0);
-        Diver.Translate(0, move * Input.GetAxis("Vertical"), 0, 0);
+        float dx = Input.GetAxis("Horizontal");
+        print(Ship.position.x);
+        
+
+        if (Ship.position.x+move*dx >= -7.8f && Ship.position.x+move*dx <= 7.73f)
+        {
+            //Ship.Translate(move * Input.GetAxis("Horizontal"), 0, 0);
+            Ship.position=new Vector3((float)Ship.position.x+move * dx, (float)Ship.position.y, (float)Ship.position.z);
+            Diver.Translate(0, move * Input.GetAxis("Vertical"), 0, 0);
+        }
+        //else Ship.position=new Vector3(Mathf.Clamp(Ship.position.x,- 5.59f, 7.73f), Ship.position.y,0);
+        
+
         //Vector3 DiverPosition = Ship.position + DiverOffset+new Vector3(0,Input.GetAxis("Vertical"),0);
         //Diver.SetPositionAndRotation(DiverPosition, Quaternion.identity);
 
